82154a4a122db3971d6d868624f3f09ecc2fb313
Dhia1111 <dhia23445@gmail.com>
2025-09-25 11:56:01 +0100

    Creating the Commen Grid for the hrm_system including language change

--- src/Component/Main/Main.tsx
+++ src/Component/Main/Main.tsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom"
 export default function Main(){
-    return <>        <Link to="/LogIn">Go to LogIn</Link>
+    return <>        <Link to="/Admine">Go to Admine</Link>
 </>
 }
